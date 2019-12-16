b0680da491ccb267c2f78908e2c121b15460691b
David Ainslie <dainslie@gmail.com>
2019-12-16 21:08:26 +0000

    Updated SocialNetworkConnectionsSpec to use a Scalatest data table instead of the ad-hoc table I started off with.

--- src/test/scala/com/backwards/social/adt/SocialNetworkConnectionsFixture.scala
+++ src/test/scala/com/backwards/social/adt/SocialNetworkConnectionsFixture.scala
@@ -4,30 +4,17 @@ import io.circe.Json
 import io.circe.literal._
 
 trait SocialNetworkConnectionsFixture {
-  object NoFacebookRelationships {
-    val harry = User("Harry")
-  }
-
-  object FacebookRelationshipCount {
-    val (john, johnFirstDegreeCount, johnSecondDegreeCount) = (User("John"), FirstDegreeCount(2), SecondDegreeCount(1))
-    val (peter, peterFirstDegreeCount, peterSecondDegreeCount) = (User("Peter"), FirstDegreeCount(3), SecondDegreeCount(0))
-    val (anna, annaFirstDegreeCount, annaSecondDegreeCount) = (User("Anna"), FirstDegreeCount(1), SecondDegreeCount(2))
-  }
-
-  import NoFacebookRelationships._
-  import FacebookRelationshipCount._
-
   val facebookSocialNetworkConnections: Json = json"""{
     "sn": "facebook",
-    "people": [{ "name": ${john.name} }, { "name": ${harry.name} }, { "name": ${peter.name} }, { "name": "George" }, { "name": "Anna" }],
+    "people": [{ "name": "John" }, { "name": "Harry" }, { "name": "Peter" }, { "name": "George" }, { "name": "Anna" }],
     "relationships": [{
-      "type": "HasConnection", "startNode": ${john.name}, "endNode": ${peter.name}
+      "type": "HasConnection", "startNode": "John", "endNode": "Peter"
     }, {
-      "type": "HasConnection", "startNode": ${john.name}, "endNode": "George"
+      "type": "HasConnection", "startNode": "John", "endNode": "George"
     }, {
-      "type": "HasConnection", "startNode": ${peter.name}, "endNode": "George"
+      "type": "HasConnection", "startNode": "Peter", "endNode": "George"
     }, {
-      "type": "HasConnection", "startNode": ${peter.name}, "endNode": "Anna"
+      "type": "HasConnection", "startNode": "Peter", "endNode": "Anna"
     }]
   }"""
 }
--- src/test/scala/com/backwards/social/adt/SocialNetworkConnectionsSpec.scala
+++ src/test/scala/com/backwards/social/adt/SocialNetworkConnectionsSpec.scala
@@ -1,35 +1,36 @@
 package com.backwards.social.adt
 
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.wordspec.AnyWordSpec
 import com.backwards.social.adt.SocialNetworkConnections._
 import com.backwards.social.adt.SocialNetworkConnectionsCodec._
 
-class SocialNetworkConnectionsSpec extends AnyWordSpec with Matchers with SocialNetworkConnectionsFixture {
+class SocialNetworkConnectionsSpec extends AnyWordSpec with Matchers with TableDrivenPropertyChecks with SocialNetworkConnectionsFixture {
   spec =>
 
-  // TODO - Should have used data tables in the following examples and avoid the extra "objects" in SocialNetworkConnectionsFixture
   "Social network connections" should {
     val Right(facebookSocialNetworkConnections) = spec.facebookSocialNetworkConnections.as[SocialNetworkConnections]
 
     "highlight who has no relationships" in {
-      noRelationships(facebookSocialNetworkConnections) mustEqual List(NoFacebookRelationships.harry)
+      noRelationships(facebookSocialNetworkConnections) mustEqual List(User("Harry"))
     }
 
-    "give first degree count for a given user" in {
-      val firstDegreeCount = SocialNetworkConnections.firstDegreeCount(_: User)(facebookSocialNetworkConnections)
+    "give degree counts for a given user" in {
+      val firstDegree = SocialNetworkConnections.firstDegreeCount(_: User)(facebookSocialNetworkConnections)
+      val secondDegree = SocialNetworkConnections.secondDegreeCount(_: User)(facebookSocialNetworkConnections)
 
-      firstDegreeCount(FacebookRelationshipCount.john) mustEqual FacebookRelationshipCount.johnFirstDegreeCount
-      firstDegreeCount(FacebookRelationshipCount.peter) mustEqual FacebookRelationshipCount.peterFirstDegreeCount
-      firstDegreeCount(FacebookRelationshipCount.anna) mustEqual FacebookRelationshipCount.annaFirstDegreeCount
-    }
+      val users = Table(
+        ("User",        "First Degree Count",   "Second Degree Count"),
+        (User("John"),  FirstDegreeCount(2),    SecondDegreeCount(1)),
+        (User("Peter"), FirstDegreeCount(3),    SecondDegreeCount(0)),
+        (User("Anna"),  FirstDegreeCount(1),    SecondDegreeCount(2))
+      )
 
-    "give second degree count for a given user" in {
-      val secondDegreeCount = SocialNetworkConnections.secondDegreeCount(_: User)(facebookSocialNetworkConnections)
-
-      secondDegreeCount(FacebookRelationshipCount.john) mustEqual FacebookRelationshipCount.johnSecondDegreeCount
-      secondDegreeCount(FacebookRelationshipCount.peter) mustEqual FacebookRelationshipCount.peterSecondDegreeCount
-      secondDegreeCount(FacebookRelationshipCount.anna) mustEqual FacebookRelationshipCount.annaSecondDegreeCount
+      forAll(users) { (user: User, firstDegreeCount: FirstDegreeCount, secondDegreeCount: SecondDegreeCount) =>
+        firstDegree(user) mustEqual firstDegreeCount
+        secondDegree(user) mustEqual secondDegreeCount
+      }
     }
   }
 }
